6c7a8d6fa7312ee12fdfb4de5c176534627a350b
dllm <2036680567@qq.com>
2026-06-11 13:04:09 +0800

    Connect resource hits to media actions

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { AlistModule } from './alist/alist.module'
 import { AuthModule } from './auth/auth.module'
 import { CliPassthroughController } from './cli/cli-passthrough.controller'
-import { ConnectedServicesController } from './connected-services.controller'
 import { DeviceModule } from './devices/device.module'
 import { HealthController } from './health.controller'
 import { MediaModule } from './media/media.module'
@@ -13,6 +12,6 @@ import { TerminalModule } from './terminal/terminal.module'
 
 @Module({
   imports: [AuthModule, DeviceModule, TerminalModule, MediaModule, ResourcesModule, StreamingGatewayModule, AlistModule, StorageModule],
-  controllers: [ConnectedServicesController, CliPassthroughController, HealthController],
+  controllers: [CliPassthroughController, HealthController],
 })
 export class AppModule {}
