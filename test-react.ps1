5e2a7a1139af500745d834226f0b7700b293aba8
chentianxiong123 <2036680567@qq.com>
2026-08-28 18:48:36 +0800

    chore: clear all source code, keep only README

--- test-react.ps1
+++
@@ -1,3 +0,0 @@
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$r = Invoke-RestMethod -Uri 'http://localhost:3000/api/chat' -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes('{"text":"打开B站"}')) -ContentType 'application/json; charset=utf-8'
-$r.data | ConvertTo-Json -Depth 5
